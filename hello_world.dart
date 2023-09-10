19b8199ff90fa6c81b088534c97a10c191f9fcf7
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-09-09 22:50:59 -0500

    feat: :sparkles: Hello World
    Hello World Dart

---
+++ hello_world.dart
@@ -0,0 +1,8 @@
+void main() {
+ // var myName = 'Rolando';
+  String myName = 'Rolando';
+  final String myWorld = 'Mundo';
+  
+  print('Hola ${myWorld.toUpperCase()}, soy un simple programador, ${myName.toLowerCase()}');
+  print('Hola ${96+2}');
+}
